5b9f7864c3074d626dd498d0197c3a9aa184d339
Wojciech <wojciech.lasak@student.uj.edu.pl>
2019-04-29 12:02:46 +0200

    general nav

--- index.php
+++ index.php
@@ -6,10 +6,17 @@
 		<meta name="viewport" content="width=device-width" />
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 		<link rel="stylesheet" type="text/css" href="./style.css" />
-		<link rel="stylesheet" type="text/css" href="./base.css" />
 	</head>
 	<body>
-		<?php include __DIR__ . '/content.php'; ?>
+		<?php $dir=array_diff(scandir(__DIR__),array(".git",".",".."));
+			$dir = array_filter($dir, function($element){
+				return is_dir(__DIR__."/".$element) && file_exists(__DIR__."/".$element."/index.php");
+			});
+			foreach ($dir as $value) {  ?>
+				<a href="<?=$value?>"><?=$value?></a>
+			<?php }
+		?>
+		
 		<script src="f3.js"></script>
 	</body>
 </html>
