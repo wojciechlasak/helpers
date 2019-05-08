ee8da14ddb3a5994dcffe4b9dacaa44872f75669
weterynarzfred <weterynarzfred@gmail.com>
2019-05-08 16:21:52 +0200

    svg path draw

---
+++ draw-path/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="./../style.css" />
+    <link rel="stylesheet" type="text/css" href="./../base.css" />
+    <link rel="stylesheet" type="text/css" href="./paths.css" />
+  </head>
+  <body>
+    <svg viewBox="0 0 100 100">
+      <path class="draw" d="M5 5C50 0 50 100 95 95" />
+      <path class="draw-regulator" d="M95 5C50 0 50 100 5 95" />
+    </svg>
+    <script src="./../f3.js"></script>
+    <script src="./../regulator/f3-regulator.js"></script>
+    <script src="./f3-sdraw.js"></script>
+  </body>
+</html>
