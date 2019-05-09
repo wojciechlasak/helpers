1951334ba85ed0404c7ed8c6dff4054e9cb91bd2
weterynarzfred <weterynarzfred@gmail.com>
2019-05-09 15:35:23 +0200

    selector

---
+++ selector/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+		<meta name="viewport" content="width=device-width" />
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="./../style.css" />
+		<link rel="stylesheet" type="text/css" href="./../base.css" />
+		<link rel="stylesheet" type="text/css" href="./selector.css" />
+	</head>
+	<body>
+		<div class="select-wrap">
+      <div class="select-option">A</div>
+      <div class="select-option">B</div>
+      <div class="select-option">C</div>
+      <div class="arrow-down">down</div>
+    </div>
+		<script src="./../f3.js"></script>
+		<script src="./f3-selector.js"></script>
+	</body>
+</html>
