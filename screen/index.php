12509e4e065aa2e8e5e026b62ee7c237e53c7212
Wojciech <wojciech.lasak@student.uj.edu.pl>
2019-05-09 13:01:15 +0200

    add scroll screens

---
+++ screen/index.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+		<meta name="viewport" content="width=device-width" />
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="../style.css" />
+		<link rel="stylesheet" type="text/css" href="base.css" />
+	</head>
+	<body>
+		<div class="flex flex-align-center flex-justify-center" id="main-container">
+      <div id="screen-container">
+        <div id="screen-prev">&lt;</div>
+        <div id="screen-next">&gt;</div>
+        <div id="screen-bar">
+          <div class="screen active">
+            <div class="column">
+              <h1>main screen</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At asperiores maiores temporibus iusto adipisci, repellendus assumenda commodi voluptatem minima, perspiciatis provident alias cupiditate debitis. Amet sint laboriosam alias officiis corporis.</p>
+              <p>Ad repellat explicabo deserunt aut cupiditate omnis sint aliquam illum vitae dignissimos laboriosam ab nihil sit eligendi earum, officia doloremque nobis necessitatibus similique mollitia doloribus velit. Ea ipsam saepe explicabo!</p>
+              <p>Delectus voluptatem aliquid harum quibusdam provident maxime nobis explicabo. Corporis saepe enim dolores soluta quo. Nulla quam hic ex, mollitia veritatis quo reprehenderit officia harum atque, reiciendis exercitationem eaque ratione!</p>
+              <p>Est, fugit temporibus ullam perspiciatis libero consequatur omnis perferendis numquam rem tempore dicta exercitationem voluptate modi repellendus cumque hic doloribus nostrum, eaque pariatur recusandae quisquam velit. Consectetur modi sint dignissimos.</p>
+              <p>Ut, natus quas quos sapiente id voluptates minus distinctio, impedit maxime placeat dolorem recusandae molestias est ipsam, fuga repellat accusamus consequuntur voluptas aliquid suscipit exercitationem? Voluptate, possimus! Placeat, tenetur deserunt.</p>
+              <p>Fugiat iusto dignissimos reiciendis voluptate corporis non quia porro, quod vel. Suscipit, delectus amet nisi eaque quaerat maxime possimus accusantium officia sit rem veniam. Repellendus unde vel reiciendis. Iure, enim.</p>
+              <p>Incidunt laboriosam optio architecto quo et, ut impedit. Voluptatem modi praesentium optio sequi asperiores quis, voluptas neque. Repudiandae suscipit numquam, voluptatum enim laudantium aperiam animi unde id saepe sed voluptatem?</p>
+              <p>Saepe eius sunt doloribus dolores, iste eos vel maxime voluptates sed numquam nobis vero nulla commodi in placeat eveniet expedita id omnis perferendis vitae odit! Eos voluptates praesentium quis error?</p>
+            </div>
+          </div>
+          <div class="screen">
+            <div class="column">
+              <h1>secondary screen</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam fugit quos soluta ducimus commodi iusto illo vero ipsa cum distinctio inventore assumenda molestiae saepe nulla nemo, architecto odio modi sequi.</p>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+		<script src="../f3.js"></script>
+		<script src="screens.js"></script>
+	</body>
+</html>
